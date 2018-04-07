28f9770ef509d110445d7ac0883c8b8e89ac6f8e
Ananda Pratim Sengupta <ananda_sengupta@infosys.com>
2018-04-07 23:29:35 +0530

    Spring Initializr Project

---
+++ src/main/java/aps/springboot/myspringstarter/MyspringstarterApplication.java
@@ -0,0 +1,12 @@
+package aps.springboot.myspringstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringstarterApplication.class, args);
+	}
+}
---
+++ src/test/java/aps/springboot/myspringstarter/MyspringstarterApplicationTests.java
@@ -0,0 +1,16 @@
+package aps.springboot.myspringstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyspringstarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
